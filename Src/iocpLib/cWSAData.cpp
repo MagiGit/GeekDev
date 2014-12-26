1b386747c69a89b445647245c86bb044823ffd46
JeongJongHyeon <wildboys07@naver.com>
2014-12-26 19:37:14 +0900

    cWSAData 추가

---
+++ Src/iocpLib/cWSAData.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "cWSAData.h"
+#include <assert.h>
+
+
+cWSAData::cWSAData()
+{
+    if( s_refCount == 0 )
+    {
+        WSADATA ws;
+        if( WSAStartup( MAKEWORD(2, 2), &ws ) != 0 )
+        {
+            // 여기서 실패하면 프로그램이 실행될 의미가 없다.
+            assert( false && "WSAStartup() error!!" );
+            exit(-1);
+        }
+    }
+
+    ++s_refCount;
+}
+
+
+cWSAData::~cWSAData()
+{
+    --s_refCount;
+
+    if( s_refCount == 0 )
+    {
+        if( WSACleanup() == SOCKET_ERROR )
+        {
+            assert( false && "WSACleanup() error!!" );
+            exit(-1);
+        }
+    }
+}
---
+++ Src/iocpLib/cWSAData.h
@@ -0,0 +1,11 @@
+#pragma once
+class cWSAData
+{
+private:
+    static int s_refCount;
+
+public:
+    cWSAData();
+    ~cWSAData();
+};
+
